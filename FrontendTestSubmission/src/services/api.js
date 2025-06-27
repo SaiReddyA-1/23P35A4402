297167e75b6d47e9ce2c53d1a648d8fd59c6831a
Sai Reddy A <saireddy@growstack.biz>
2025-06-27 10:08:17 +0530

    first commit

--- FrontendTestSubmission/src/services/api.js
+++ FrontendTestSubmission/src/services/api.js
@@ -1,7 +1,8 @@
 const BASE_URL = 'http://localhost:5000';
+const DEPLOYED_URL = 'https://affordableshortner.onrender.com';
 
 async function createShortUrl(url, shortcode, validity) {
-  const res = await fetch(`${BASE_URL}/shorturls`, {
+  const res = await fetch(`${DEPLOYED_URL}/shorturls`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ url, shortcode, validity })
@@ -12,14 +13,14 @@ async function createShortUrl(url, shortcode, validity) {
 }
 
 async function getStats(shortcode) {
-  const res = await fetch(`${BASE_URL}/shorturls/${shortcode}`);
+  const res = await fetch(`${DEPLOYED_URL}/shorturls/${shortcode}`);
   const data = await res.json();
   if (!res.ok) throw data;
   return data;
 }
 
 async function getAllShortUrls() {
-  const res = await fetch(`${BASE_URL}/shorturls`);
+  const res = await fetch(`${DEPLOYED_URL}/shorturls`);
   const data = await res.json();
   if (!res.ok) throw data;
   return data;
